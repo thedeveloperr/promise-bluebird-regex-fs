57cadaf67cb6231d12c3aaf128c4adc50ba0fcec
Mohit Gupta <mgupta@localhost.localdomain>
2017-06-07 21:18:22 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Promise = require("bluebird");
+const fs = require("fs");
+Promise.promisifyAll(fs);
+fs.readFileAsync('data.txt','utf-8')
+.then( data =>{
+  return new Promise((resolve, reject) => {
+    const re = /\+91\d{10}(?![0-9])/g;
+    resolve(data.match(re));
+  });
+})
+.then( arr =>{
+    return fs.writeFileAsync('phoneNum.txt',arr.toString());
+})
+.then( () =>{
+    return fs.readFileAsync('phoneNum.txt','utf-8');  
+})
+.then( data =>{
+  console.log("Created a file phoneNum.txt Plz check it using command: cat phoneNum.txt");
+})
+.catch( err =>{
+    console.log(err);
+});
